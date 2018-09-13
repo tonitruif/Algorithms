ac6545d179d2c0ce245216e4fea99c48e7fda81e
Daria Firsova <tonitruif@gmail.com>
2018-09-14 01:07:22 +0300

    working demerau and recursion

--- lab1/functions.py
+++ lab1/functions.py
@@ -4,10 +4,10 @@ def levenstein(str1, str2):
     if (l1 == 0) and (l2 == 0):
         return 0
 
-    if ((l1 == 0) and (l2 > 0)):
+    if (l1 == 0) and (l2 > 0):
         return l2
 
-    if ((l1 > 0) and (l2 == 0)):
+    if (l1 > 0) and (l2 == 0):
         return l1
 
     mtr = [[0 for x in range(l2)] for y in range(l1)]
@@ -16,18 +16,73 @@ def levenstein(str1, str2):
             mtr[0][j] = j
             mtr[i][0] = i
 
-    for i in range(1,l1):
-        for j in range(1,l2):
-            if (str1[i] == str2[j]):
+    for i in range(1, l1):
+        for j in range(1, l2):
+            if str1[i] == str2[j]:
                 flagmatch = 0
             else:
                 flagmatch = 1
-            mtr[i][j] = min(mtr[i-1][j]+1, mtr[i][j-1]+1, mtr[i-1][j-1] + flagmatch)
+            mtr[i][j] = min(mtr[i - 1][j] + 1, mtr[i][j - 1] + 1, mtr[i - 1][j - 1] + flagmatch)
 
     for i in range(l1):
         for j in range(l2):
-            print(mtr[i][j], end = ' ')
+            print(mtr[i][j], end=' ')
+        print()
+
+    return mtr[i][j]
+
+
+def demerau(str1, str2):
+    l1, l2 = len(str1), len(str2)
+
+    if (l1 == 0) and (l2 == 0):
+        return 0
+
+    if (l1 == 0) and (l2 > 0):
+        return l2
+
+    if (l1 > 0) and (l2 == 0):
+        return l1
+
+    mtr = [[0 for x in range(l2)] for y in range(l1)]
+    for i in range(l1):
+        for j in range(l2):
+            mtr[0][j] = j
+            mtr[i][0] = i
+
+    for i in range(1, l1):
+        for j in range(1, l2):
+            if str1[i] == str2[j]:
+                flagmatch = 0
+            else:
+                flagmatch = 1
+
+            if (i > 1) and (j > 1) and (str1[i] == str2[j - 1]) and (str1[i - 1] == str2[j]):
+                mtr[i][j] = min(mtr[i - 1][j] + 1, mtr[i][j - 1] + 1, mtr[i - 1][j - 1] + flagmatch, mtr[i - 2][j - 2])
+            else:
+                mtr[i][j] = min(mtr[i - 1][j] + 1, mtr[i][j - 1] + 1, mtr[i - 1][j - 1] + flagmatch)
+
+        # mtr[i][j] = min(mtr[i - 1][j] + 1, mtr[i][j - 1] + 1, mtr[i - 1][j - 1] + flagmatch, mtr[])
+
+    for i in range(l1):
+        for j in range(l2):
+            print(mtr[i][j], end=' ')
         print()
     return mtr[i][j]
+
+
+def recursion(str1, str2):
+    l1, l2 = len(str1), len(str2)
+    if l1 == 0 or l2 == 0:
+        return max(l1,l2)
+    if str1[-1] == str2[-1]:
+        flagmatch = 0
+    else:
+        flagmatch = 1
+
+    result = min([recursion(str1[:-1], str2) + 1, recursion(str1, str2[:-1]) + 1, recursion(str1[:-1], str2[:-1]) + flagmatch])
+
+    return result
+
 
 
--- lab1/main.py
+++ lab1/main.py
@@ -2,16 +2,31 @@ from lab1 import functions as func
 import time
 
 if __name__ == "__main__":
+    '''
     str1 = str(input("enter string1: "))
     str2 = str(input("enter string2: "))
-    str2 = 'l'+ str2
-    str1 = 'l'+ str1
+    '''
+    str1 = 'tartaara'
+    str2 = 'otafra'
+    str2 = 'l' + str2
+    str1 = 'l' + str1
     if str1 > str2:
         str1, str2 = str2, str1
 
     start = time.time()
-    basic_levenstein =  func.levenstein(str1, str2)
+    basic_levenstein = func.levenstein(str1, str2)
+    print('RESULT BASIC: ',basic_levenstein)
     end = time.time() - start
+    print(end * 10000,'\n')
 
-    print(end *1000)
+    start = time.time()
+    demerau = func.demerau(str1, str2)
+    print('RESULT DEMERAU: ', demerau)
+    end = time.time() - start
+    print(end * 10000)
 
+    start = time.time()
+    recursion = func.recursion(str1, str2)
+    print('RESULT RECURSION: ', recursion)
+    end = time.time() - start
+    print(end * 10000)
