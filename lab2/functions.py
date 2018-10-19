b23193d9158bc314dac87e4f001c72a3c7400580
Daria Firsova <tonitruif@gmail.com>
2018-10-20 00:08:38 +0300

    opt winograd

--- lab2/functions.py
+++ lab2/functions.py
@@ -13,23 +13,56 @@ def winograd(G, H):
     c = len(H[0])
 
     d = b // 2
-    row_factor = [0 for i in range(a)]
-    column_factor = [0 for i in range(c)]
+    mulU = [0 for i in range(a)]
+    mulV = [0 for i in range(c)]
+    R = [[0 for i in range(c)] for j in range(a)]
+
+    for i in range(a):
+        for j in range(0, b // 2, 1):
+            mulU[i] = mulU[i] + G[i][2 * j] * G[i][2 * j + 1]
+
+    for i in range(c):
+        for j in range(0, b // 2, 1):
+            mulV[i] = mulV[i] + H[2 * j][i] * H[2 * j + 1][i]
+
+    for i in range(a):
+        for j in range(c):
+            R[i][j] = - mulU[i] - mulV[j]
+            for k in range(0, b // 2, 1):
+                R[i][j] = R[i][j] + ((G[i][2 * k] + H[2 * k + 1][j]) * (G[i][2 * k + 1] + H[2 * k][j]))
+
+    if b % 2:
+        for i in range(a):
+            for j in range(c):
+                R[i][j] = R[i][j] + G[i][b - 1] * H[b - 1][j]
+
+    return R
+
+
+def opt_winograd(G, H):
+    a = len(G)
+    b = len(H)
+    c = len(H[0])
+
+    d = b // 2
+    mulU = [0 for i in range(a)]
+    mulV = [0 for i in range(c)]
     R = [[0 for i in range(c)] for j in range(a)]
 
     for i in range(a):
         for j in range(d):
-            row_factor[i] = row_factor[i] + G[i][2 * j] * G[i][2 * j + 1]
+            mulU[i] += G[i][2 * j] * G[i][2 * j + 1]
 
     for i in range(c):
         for j in range(d):
-            column_factor[i] = column_factor[i] + H[2 * j][i] * H[2 * j + 1][i]
+            mulV[i] += H[2 * j][i] * H[2 * j + 1][i]
 
     for i in range(a):
         for j in range(c):
-            R[i][j] = - row_factor[i] - column_factor[j]
+            R[i][j] = - mulU[i] - mulV[j]
             for k in range(d):
-                R[i][j] += ((G[i][2 * k] + H[2 * k + 1][j]) * (G[i][2 * k + 1] + H[2 * k][j]))
+                index = 2 * k
+                R[i][j] += ((G[i][index] + H[index + 1][j]) * (G[i][index + 1] + H[index][j]))
 
     if b % 2:
         for i in range(a):
--- lab2/main.py
+++ lab2/main.py
@@ -3,17 +3,19 @@ from random import randint
 import numpy as np
 
 if __name__ == '__main__':
+    f = open('out.txt', 'w')
+    for i in range():
+        
     M,N,Q = 2, 3, 2
     a = [[randint(0,25) for i in range(N)] for j in range(M)]
     b = [[randint(0,25) for i in range(Q)] for j in range(N)]
-    #a = [[2,-3,1],[5,4,-2]]
-    #b = [[-7,5], [2,-1],[4,3]]
-    print(a)
-    print(b)
+
     res = func.multiply(a, b, M, N, Q)
     print(res)
     result_wino = func.winograd(a,b)
     print(result_wino)
+    result_opt = func.opt_winograd(a,b)
+    print(result_opt)
     num = np.matmul(a,b)
     print(num)
 
